3493402fee4a4cf7206ee26e0842370f04687a6c
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-07 17:01:55 +0100

    Formatting

--- JsonEditor/test/propertyCheckerTests.js
+++ JsonEditor/test/propertyCheckerTests.js
@@ -4,7 +4,7 @@ var checker = require('../src/propertyChecker');
 describe('Check if property exists', () => {
   it('Property exists', function () {
     const obj = { name: 'John' };
-    const property="name";
+    const property = 'name';
 
     const result = checker.doesExist(obj, property);
 
@@ -13,19 +13,19 @@ describe('Check if property exists', () => {
 
   it('Property does not exist', function () {
     const obj = { name: 'John' };
-    const property="last_name";
+    const property = 'last_name';
 
     const result = checker.doesExist(obj, property);
-    
+
     result.should.be.false();
   });
 
   it('Property exists but contain null', function () {
     const obj = { name: 'John', second_name: null };
-    const property="second_name";
+    const property = 'second_name';
 
     const result = checker.doesExist(obj, property);
-    
+
     result.should.be.true();
   });
 });
