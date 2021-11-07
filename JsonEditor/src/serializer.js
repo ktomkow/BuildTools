1e145cd6564e3d337aeba08ff1093d17f3b68f5a
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-07 16:11:46 +0100

    Remove example test, add other tests, filter comments

--- JsonEditor/src/serializer.js
+++ JsonEditor/src/serializer.js
@@ -1,9 +1,25 @@
-const serialize = (object) => {
-
-}
+const serialize = (object) => {};
 
 const deserialize = (json) => {
-  return JSON.parse(json);
-}
+  const lines = splitToLines(json);
+  const formattedLines = removeComments(lines);
+  const joinedLines = joinLines(formattedLines);
+
+  return JSON.parse(joinedLines);
+};
+
+const splitToLines = (json) => {
+  return json.split('\r\n');
+};
+
+const joinLines = (lines) => {
+  return lines.join('');
+};
+
+const removeComments = (lines) => {
+  return lines.map((x) => {
+    return x.split(" // ")[0];
+  })
+};
 
 module.exports = { serialize, deserialize };
