958931c192af2e76de37509d4ab67c90b5e35b73
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-10 23:14:54 +0100

    More deserializer tests

--- JsonEditor/test/serializerTests.js
+++ JsonEditor/test/serializerTests.js
@@ -8,4 +8,19 @@ describe('Deserialize object from string', () => {
 
     instance.should.have.property('someText', 'Yes this is text');
   });
+
+  it('One number array property json', function () {
+    const json = '{"arr" : [1,2,3]}';
+    const instance = serializer.deserialize(json);
+
+    instance.arr.should.be.instanceof(Array).and.have.lengthOf(3);
+  });
+
+  it('One nested number property', function () {
+    const json = '{"numbers" : { "first": 1, "second": 2}}';
+    const instance = serializer.deserialize(json);
+
+    instance.numbers.first.should.be.instanceof(Number).and.be.exactly(1);
+    instance.numbers.second.should.be.instanceof(Number).and.be.exactly(2);
+  });
 });
