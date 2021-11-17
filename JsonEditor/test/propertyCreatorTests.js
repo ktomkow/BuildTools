45c34e7e24ff7eb4d91e60f228fe9becf9ff72bc
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-17 22:49:31 +0100

    Reorganize code structure

--- JsonEditor/test/propertyCreatorTests.js
+++ JsonEditor/test/propertyCreatorTests.js
@@ -4,23 +4,35 @@ var propertyChecker = require('../src/propertyChecker');
 
 describe('Create property', () => {
   describe('Property not exists', () => {
-    it('Returns not null and not undefined', function () {
-      const obj = {};
-      const property = 'anyway';
+    describe('Simples, not nested property', () => {
+      it('Returns not null and not undefined', function () {
+        const obj = {};
+        const property = 'anyway';
 
-      const newObj = creator.create(obj, property);
+        const newObj = creator.create(obj, property);
 
-      const objNotNull = !!newObj;
-      objNotNull.should.be.true();
-    });
-    it('Returns object and create property', function () {
-      const obj = {};
-      const property = 'meme';
+        const objNotNull = !!newObj;
+        objNotNull.should.be.true();
+      });
+      it('Returns object and create property', function () {
+        const obj = {};
+        const property = 'meme';
 
-      const newObj = creator.create(obj, property);
+        const newObj = creator.create(obj, property);
 
-      const memeExists = propertyChecker.doesExist(newObj, property);
-      memeExists.should.be.true();
+        const memeExists = propertyChecker.doesExist(newObj, property);
+        memeExists.should.be.true();
+      });
+
+      it('Create object and create property with null', function () {
+        const obj = {};
+        const property = 'meme';
+
+        const newObj = creator.create(obj, property);
+
+        const memeExists = propertyChecker.doesExist(newObj, property);
+        memeExists.should.be.true();
+      });
     });
   });
   describe('Property already exist', () => {
@@ -30,7 +42,6 @@ describe('Create property', () => {
 
       const newObj = creator.create(obj, property);
 
-      const propertyNotNull = !!newObj.prop;
       const propertyExists = propertyChecker.doesExist(newObj, property);
 
       propertyExists.should.be.true();
