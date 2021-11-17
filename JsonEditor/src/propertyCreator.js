a1066ad4ac7ef72082b3b65569ae887eeff6ac55
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-18 00:28:53 +0100

    Code formatting

--- JsonEditor/src/propertyCreator.js
+++ JsonEditor/src/propertyCreator.js
@@ -3,7 +3,6 @@ var propertyChecker = require('./propertyChecker');
 
 // Immutable
 create = (object, key) => {
-  
   const result = !!object ? clone(object) : {};
 
   if (propertyChecker.doesExist(object, key) === false) {
